8f846a3a4bd1d353b0f03665eaa44a7861727332
zoevonallmen <z.vonallmen@gmail.com>
2025-04-10 12:35:03 +0200

    updated code to get a separate variable for each task in the original dataset, tested for two articles

--- GPT API/GPT API.R
+++ GPT API/GPT API.R
@@ -34,7 +34,7 @@ classify_article <- function(article_text, prompt_text) {
   return(result$choices$message$content)
 }
 
-test <- classify_article(article_text = articles$content[1], prompt_text = prompt)
+test <- classify_article(article_text = articles$content[159], prompt_text = prompt)
 
 test$choices$message$content
 
@@ -42,12 +42,64 @@ test$choices$message$content
 #Test for 10 articles ----------------------------------------------------------
 classified_results <- vector("list", length = 10)
 
-for (i in 151:160) {
+for (i in 151:152) {
   article_text <- articles$content[i]  
   
   classified_results[[i]] <- classify_article(article_text = article_text, prompt_text = prompt)
 }
 
+
+
 classified_results <- unlist(classified_results)
 
 
+write.csv(classified_results, "classified_articles_150_to_160.csv", row.names = FALSE)
+
+test <- read_csv("classified_articles_150_to_160.csv")
+
+
+#testing fro two articles, separating tasks -----------------------------------------
+
+relevance <- vector("character", length = 2)
+indirect <- vector("character", length = 2)
+frame <- vector("character", length = 2)
+sentiment <- vector("character", length = 2)
+opposition <- vector("character", length = 2)
+
+# Loop through just the first 2 articles
+for (i in 1:2) {
+  article_text <- articles$content[i] 
+  
+
+  task_results <- classify_article(article_text = article_text, prompt_text = prompt)
+  
+
+  task_values <- strsplit(task_results, "\n")[[1]]  
+  
+
+  task_values <- trimws(task_values)
+  
+
+  if (length(task_values) == 5) {
+
+    relevance[i] <- task_values[1]  # First task (relevance)
+    indirect[i] <- task_values[2]   # Second task (indirect)
+    frame[i] <- task_values[3]      # Third task (frame)
+    sentiment[i] <- task_values[4]  # Fourth task (sentiment)
+    opposition[i] <- task_values[5] # Fifth task (opposition)
+  } else {
+
+    cat("Error: Unexpected number of tasks in article", i, "\n")
+  }
+  
+}
+
+articles$relevance[1:2] <- relevance
+articles$indirect[1:2] <- indirect
+articles$frame[1:2] <- frame
+articles$sentiment[1:2] <- sentiment
+articles$opposition[1:2] <- opposition
+
+
+write.csv(articles[1:2, ], "classified_articles_with_tasks_2_articles.csv", row.names = FALSE)
+help <- read.csv("classified_articles_with_tasks_2_articles.csv")
