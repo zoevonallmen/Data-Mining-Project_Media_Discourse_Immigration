2df627b753f6f78b076b42f30c36f94678db15b6
zoevonallmen <z.vonallmen@gmail.com>
2025-04-15 20:51:49 +0200

    Handling NA's

--- Data Analysis/Data Analysis.R
+++ Data Analysis/Data Analysis.R
@@ -7,8 +7,6 @@ ArticlesClassified <- read_csv("Data/final_classified_articles.csv") #Classified
 Joined_Data <- Articles |> 
   left_join(ArticlesClassified, by = "id")
 
-write_csv(Joined_Data, "Data/analysis_data_merged.csv")
-
 #Data inspection ---------------------------------------------------------------
 glimpse(Joined_Data)
 summary(Joined_Data) #706 NA's 
@@ -18,3 +16,17 @@ colSums(is.na(Joined_Data))
 Joined_Data |> 
   select(starts_with("code")) |> 
   map(table)
+
+#Handling NA's ----------------------------------------------------------------
+Joined_Data_filtered <- Joined_Data |> 
+  filter(!if_any(starts_with("code"), is.na))
+
+nrow(Joined_Data_filtered) #N = 1025, 706 NA's removed
+
+write_csv(Joined_Data_filtered, "Data/analysis_data_merged.csv")
+
+#Task 1 - Relevance: remove irrelevant articles
+Joined_Data_filtered <- Joined_Data |> 
+  filter(code_1 == 1) #772 obs left
+
+write_csv(Joined_Data_filtered, "Data/analysis_data_relevant_only.csv")
