ec7cf1e4ab7e75a4c99e065b022e9d4f4bc02f94
zoevonallmen <z.vonallmen@gmail.com>
2025-04-15 20:36:01 +0200

    Script for Data Analysis, merged original data and classification data

---
+++ Data Analysis/Data Analysis.R
@@ -0,0 +1,10 @@
+#Merge Datasets ---------------------------------------------------------------
+library(tidyverse)
+
+Articles <- read_csv("Data/Final_Articles.csv") #Swissdox API cleaned dataset
+ArticlesClassified <- read_csv("Data/final_classified_articles.csv") #Classified Data
+
+Joined_Data <- Articles |> 
+  left_join(ArticlesClassified, by = "id")
+
+write_csv(Joined_Data, "Data/analysis_data_merged.csv")
